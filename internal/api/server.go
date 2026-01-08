63c27bff3bcf3a8487e5f49892372f9ff7ae6091
Aaron <aaron@paragonescape.com>
2026-01-08 16:06:44 +0000

    feat: add minimal operator UI at /ui

--- internal/api/server.go
+++ internal/api/server.go
@@ -258,6 +258,7 @@ func ListenAndServe(port int) error {
 	mux.HandleFunc("/game/start", gameStartHandler)
 	mux.HandleFunc("/game/stop", gameStopHandler)
 	mux.HandleFunc("/ws/events", wsEventsHandler)
+	mux.HandleFunc("/ui", uiHandler)
 
 	addr := fmt.Sprintf(":%d", port)
 	log.Printf("API listening on %s\n", addr)
